bc2a4e2fad5a334d55c67e0cd55a0782deede59b
JeanVittory <vittory.dev@gmail.com>
2024-03-13 11:32:06 -0500

    feat: :art: An email regex was cofigured in joi validation
    An email regex validations was configured in the joi validation at authentication process

--- src/api/validators/auth.validators.ts
+++ src/api/validators/auth.validators.ts
@@ -1,6 +1,8 @@
 import Joi from 'joi';
 
 export const authenticationSchema = Joi.object({
-	email: Joi.string().required(),
+	email: Joi.string()
+		.required()
+		.regex(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
 	password: Joi.string().required().max(16),
 });
