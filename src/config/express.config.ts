c4979b121c45c5cad9929e0a8318edde0c65188e
JeanVittory <vittory.dev@gmail.com>
2024-03-13 11:16:55 -0500

    feat: :goal_net: The handler error was improved
    The handler error now is using a .json file with centralized code and message errors. The application is already using it

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -1,7 +1,13 @@
-import express from "express"
-import { routesConfiguration } from "./router.config"
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { routesConfiguration } from './router.config';
+import { errorHandler } from './errors/errorHandler.config';
 
-export const app = express()
+export const app = express();
 
-app.use(express.json())
-routesConfiguration()
+app.use(express.json());
+routesConfiguration();
+app.use(errorHandler);
+app.use((_err: Error, _req: Request, res: Response, _next: NextFunction): void => {
+	res.status(500).send('Internal Server Error');
+});
